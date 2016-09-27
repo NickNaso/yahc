6fe4e4f41882ac8633c2f3737f8b7d8ef2364b7e
NickNaso <nicoladelgobbo@gmail.com>
2016-09-28 01:30:40 +0200

    Scaffolding for test

--- test/yahc.js
+++ test/yahc.js
@@ -1,6 +1,6 @@
 
 /*******************************************************************************
- * Copyright (c) 2016 Mauro Doganieri - Nicola Del Gobbo
+ * Copyright (c) 2016 Nicola Del Gobbo
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the license at http://www.apache.org/licenses/LICENSE-2.0
@@ -14,7 +14,6 @@
  * permissions and limitations under the License.
  *
  * Contributors - initial API implementation:
- * Mauro Doganieri <mauro.doganieri@gmail.com>
  * Nicola Del Gobbo <nicoladelgobbo@gmail.com>
  ******************************************************************************/
 
@@ -23,4 +22,139 @@
 /*!
  * Module dependencies
  */
+const bodyParser = require('body-parser');
+const express = require('express');
+const formidable = require('formidable');
+const fs = require('fs-extra');
 const HttpClient = require('../');
+
+describe("Test yahc", function () {
+
+  // Set express app to test the rest client
+  const app = express();
+  app
+  .use(
+    bodyParser
+    .json(
+      {
+        strict: true,
+        inflate: true,
+        limit: "5mb"
+      }
+    )
+  );
+  app
+  .use(
+    bodyParser
+    .urlencoded(
+      {
+        extended: false,
+        inflate: true, 
+        limit: "5mb",
+        parameterLimit: 1000
+      }
+    )
+  );
+  app.get("/get", function (req, res) {
+
+  });
+  app.get("/get-json", function (req, res) {
+
+  });
+  app.post("/post", function (req, res) {
+
+  });
+  app.post("/post-json", function (req, res) {
+
+  });
+  app.post("/post-form-data", function (req, res) {
+
+  });
+  app.put("/put", function (req, res) {
+
+  });
+  app.put("/put-json", function (req, res) {
+
+  });
+  app.put("/put-form-data", function (req, res) {
+
+  });
+  app.delete("/delete", function (req, res) {
+
+  });
+  app.delete("/delete-json", function (req, res) {
+
+  });
+
+
+  it(
+    "GET - application/x-www-form-urlencoded", 
+    function (done) {
+      done();
+    }
+  );
+
+  it(
+    "GET - application/x-www-form-urlencoded - json", 
+    function (done) {
+      done();
+    }
+  );
+
+  it(
+    "POST - application/x-www-form-urlencoded", 
+    function (done) {
+      done();
+    }
+  );
+
+  it(
+    "POST - application/x-www-form-urlencoded - json", 
+    function (done) {
+      done();
+    }
+  );
+
+  it(
+    "POST - multipart/form-data", 
+    function (done) {
+      done();
+    }
+  );
+
+  it(
+    "PUT - application/x-www-form-urlencoded", 
+    function (done) {
+      done();
+    }
+  );
+
+  it(
+    "PUT -  application/x-www-form-urlencoded - json", 
+    function (done) {
+      done();
+    }
+  );
+
+  it(
+    "PUT - multipart/form-data", 
+    function (done) {
+      done();
+    }
+  );
+
+  it(
+    "DELETE - application/x-www-form-urlencoded", 
+    function (done) {
+      done();
+    }
+  );
+
+  it(
+    "DELETE - application/x-www-form-urlencoded - json", 
+    function (done) {
+      done();
+    }
+  );
+
+});
